4d74f4f0113e41548dd92046236b6f0398c2baab
Tambi <tambi.asawo@yahoo.com>
2023-11-12 00:20:13 -0800

    Update characterItem.js

--- src/components/characters/characterItem.js
+++ src/components/characters/characterItem.js
@@ -1,27 +1,33 @@
 import React from "react";
+import Tooltip from "@mui/material/Tooltip";
 
 const characterItem = ({ item }) => {
-  //console.log(item);
   return (
     <div className="card">
       <div className="card-inner">
         <div className="card-front">
-          <img src={item.img} alt="" />
+          <img
+            src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
+            alt={item.original_title}
+          />
         </div>
         <div className="card-back">
-          <h1>{item.name}</h1>
+          <h1>{item.original_title}</h1>
           <ul>
+            {/* <li>
+              <strong>Overview:</strong>{" "}
+              <Tooltip title={item.overview}>
+                <p style={{ textOverflow: "ellipsis" }}>{item.overview}</p>
+              </Tooltip>
+            </li> */}
             <li>
-              <strong>Actor Name:</strong> {item.portrayed}
+              <strong>Language:</strong> {item.original_language.toUpperCase()}
             </li>
             <li>
-              <strong>Nickname:</strong> {item.nickname}
+              <strong>Adult Content:</strong> {item.adult ? "Yes" : "No"}
             </li>
             <li>
-              <strong>Birthday:</strong> {item.birthday}
-            </li>
-            <li>
-              <strong>Status:</strong> {item.status}
+              <strong>Release Date:</strong> {item.release_date}
             </li>
           </ul>
         </div>
