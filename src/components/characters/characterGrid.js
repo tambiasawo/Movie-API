190df1c721dc767fce18cef1f5592c9a06cdbae5
Tambi <tambi.asawo@yahoo.com>
2023-11-12 00:19:48 -0800

    Update characterGrid.js

--- src/components/characters/characterGrid.js
+++ src/components/characters/characterGrid.js
@@ -2,13 +2,13 @@ import React from "react";
 import CharacterItem from "./characterItem";
 import Spinner from "../ui/spinner";
 const characterGrid = ({ isLoading, items }) => {
-  //used destructuring here
+
   return isLoading ? (
     <Spinner />
   ) : (
     <section className="cards">
       {items.map((item) => (
-        <CharacterItem key={item.char_id} item={item}></CharacterItem>
+        <CharacterItem key={item.id} item={item}></CharacterItem>
       ))}
     </section>
   );
