6934d985f6a9f6ed0beb727de39384c88a7a816e
Tambi <tambi.asawo@yahoo.com>
2023-11-12 00:19:11 -0800

    Update logo

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -4,7 +4,12 @@ import logo from "../../img/logo.png";
 const Header = () => {
   return (
     <header className="center">
-      <img src={logo} alt="" />
+      <img
+        src={
+          "https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=2918&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+        alt="Movie DB"
+      />
     </header>
   );
 };
